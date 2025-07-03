4f7f26ff5947955b5f54eeb2dd7df99abb8c9f5a
wutdahack1337 <wutdahack1337@gmail.com>
2025-07-03 14:30:14 +0700

    chore: only use RF prediction for web application

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 Applied of Machine Learning project
 
 ```
-unzip models/RF_100.zip
-unzip models/RF_300.zip
+cd models
+unzip RF_100.zip
+unzip RF_300.zip
+cd ..
+```
+
+```
 python3 scripts/train.py
 python3 app/predict.py
 ```
--- app/predict.py
+++ app/predict.py
@@ -1,36 +1,37 @@
 import joblib
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
 from flask import Flask, render_template, request
 
-models = {
-    "LR": joblib.load("models/LinearRegression(n_jobs=-1).pkl"),
-    # TODO: change n_estimator
-    "RF": joblib.load("models/RF_5.pkl"),
-    # TODO: change n_estimator
-    "GBM": joblib.load("models/GBM_5.pkl"),
-}
+model = joblib.load("models/RF_300.pkl")
+
+# Test models
+print("######### Testing model to see whether it work #########")
+testing_df = pd.read_csv("datasets/testing.csv")
+testing_df.set_index("date", inplace=True)
+X_test = testing_df.iloc[:, 1:]
+y_test = testing_df.Appliances
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+print("   -> RMSE", np.round(rmse, 3))
+print("##################")
 
 app = Flask(__name__)
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     features   = ["lights", "T1", "RH_1", "T2", "RH_2", "T3", "RH_3", "T4", "RH_4", "T5", "RH_5", "T6", "RH_6", "T7", "RH_7", "T8", "RH_8", "T9", "RH_9", "T_out", "Press_mm_hg", "RH_out", "Windspeed", "Visibility", "Tdewpoint"]
-    prediction_LR   = None
-    prediction_RF   = None
-    prediction_GBM  = None
-    mean_prediction = None
+    prediction   = None
     input_data = []
     if request.method == 'POST':
         for feature in features:
             input_data.append(float(request.form[feature]))
         
         input_data = [input_data]
+        prediction = model.predict(input_data)[0]
 
-        prediction_LR = models["LR"].predict(input_data)[0]
-        prediction_RF = models["RF"].predict(input_data)[0]
-        prediction_GBM = models["GBM"].predict(input_data)[0]
-        mean_prediction = (prediction_LR + prediction_RF + prediction_GBM)/3
-
-    return render_template('index.html', prediction_LR=prediction_LR, prediction_RF=prediction_RF, prediction_GBM=prediction_GBM, mean_prediction=mean_prediction)
+    return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
